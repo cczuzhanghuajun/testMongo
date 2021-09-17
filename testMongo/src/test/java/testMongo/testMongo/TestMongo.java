dec22eeeba7d0c666200132715c2ca845d6d5663
cczuzhanghuajun <zhang.huajun@cczu.edu.cn>
2021-09-17 15:05:30 +0800

    lala

---
+++ testMongo/src/test/java/testMongo/testMongo/TestMongo.java
@@ -0,0 +1,23 @@
+package testMongo.testMongo;
+
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+
+public class TestMongo {
+
+	public static void main(String[] args) {
+		String uri = "mongodb://127.0.0.1:27017";
+	    try (MongoClient mongoClient = MongoClients.create(uri)) {
+	        MongoDatabase database = mongoClient.getDatabase("aaa");
+	        MongoCollection<Document> collection = database.getCollection("lala");
+	        Document doc = collection.find().first();
+	        System.out.println(doc.toJson());
+	    }
+
+	}
+
+}
